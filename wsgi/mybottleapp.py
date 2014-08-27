a5f54c0c35406bded381c120dd5bbd80089b1252
kallimachos <kallimachos@gmail.com>
2014-08-27 12:06:54 +1000

    Fix template path and add sample template

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,13 +1,26 @@
-from bottle import route, default_app
+from bottle import route, default_app, template
+
+# -------------------------------------------------------------------------------------
+# This must be added in order to do correct path lookups for templates in wsgi/views/
+import os
+from bottle import TEMPLATE_PATH
+
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'],
+    'wsgi/views/'))
+# -------------------------------------------------------------------------------------
 
 @route('/')
 def index():
     return '<strong>Hello World!</strong>'
 
-# This must be added in order to do correct path lookups for the views
-import os
-from bottle import TEMPLATE_PATH
-TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_HOMEDIR'],
-    'runtime/repo/wsgi/views/'))
+# example route using a template
+@route('/hello/<name>')
+def index(name):
+    return template('hello_name', name=name)
+
+# route for printing debug information
+@route('/debug')
+def index():
+    return template('<b>TEMPLATE_PATH = {{info}}', info=TEMPLATE_PATH)
 
 application=default_app()

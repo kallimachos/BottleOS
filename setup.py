0b2f36189542ef64617bf07568d4a2296c78d758
kallimachos <kallimachos@gmail.com>
2015-11-14 19:59:17 +1000

    pep8 compliance

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ setup(name='YourAppName',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['bottle'],
-     )
+      )
--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,26 +1,28 @@
 from bottle import route, default_app, template
 
-# -------------------------------------------------------------------------------------
-# This must be added in order to do correct path lookups for templates in wsgi/views/
+# -----------------------------------------------------------------------------
+# This must be added in order to do correct path lookups for templates in
+# wsgi/views/
 import os
 from bottle import TEMPLATE_PATH
 
 TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'],
-    'wsgi/views/'))
-# -------------------------------------------------------------------------------------
+                                  'wsgi/views/'))
+# -----------------------------------------------------------------------------
+
 
 @route('/')
 def index():
     return '<strong>Hello World!</strong>'
 
-# example route using a template
+
 @route('/hello/<name>')
-def index(name):
+def index(name):  # example route using a template
     return template('hello_name', name=name)
 
-# route for printing debug information
+
 @route('/debug')
-def index():
+def index():  # route for printing debug information
     return template('<b>TEMPLATE_PATH = {{info}}', info=TEMPLATE_PATH)
 
-application=default_app()
+application = default_app()
